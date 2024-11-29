dfee39f74773a714ad995362d8a780083211f4ca
Abdo <abdonabil713@gmail.com>
2024-11-29 10:59:09 +0200

    chains

---
+++ Js/landingPage/products.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function () {
+    
+        $(".owl-carousel").owlCarousel({
+            items:1,
+            dots:false
+        });
+
+
+    // $('body').on('click','.productsContainer .likeBtn',function(){
+    //     $(this).toggleClass('liked')
+    //     if($(this).hasClass('liked')){
+    //         $(this).find('.fa-regular').addClass('fa');
+    //         $(this).find('.fa-regular').removeClass('fa-regular');
+            
+    //     }
+    //     else{
+    //         $(this).find('.fa').addClass('fa-regular');
+    //         $(this).find('.fa').removeClass('fa');
+    //     }
+        
+    // })
+})
